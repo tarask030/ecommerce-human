8bf29307b0357627732f14fcb31b73b5960bf3da
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-05-04 19:45:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 <html>
  <head>
     <meta charset="utf-8" />
-    <title>HumanIT</title>
+    <title>landing page</title>
     <link rel="stylesheet" href= "style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com"> 
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&displa>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
 
     <div class= "header">
-      <a href="https://www.ecommerce.humanit.group/">
+      <a href="Index.html">
         <div class="logo_main"><img src="images/humanit_lable.svg" alt="image"></div>
      </a>
       <div class="slogan_main">
-          <h1 class="slogan_main_text"><strong>SKLEP INTERNETOWY</strong></br> SZYTY NA MIA>
+          <h1 class="slogan_main_text"><strong>SKLEP INTERNETOWY</strong></br> SZYTY NA MIARĘ</h1>
       </div>
       <div class="img_comp"><img src="images/web_page_comp.svg" alt=""></div>
       <div class="line_right_first"><img src="images/line.svg" alt=""></div>
@@ -26,24 +26,25 @@
       <div class="sector_bag">
         <h3 class="title_text_right">Sprzedajesz to, co chcesz i jak chcesz</h3>
         <div class="description_text_right">
-          <p>Sklep internetowy, który zaprojektujemy i stworzymy <em>specjalnie dla Ciebie<>
-              Po zalogowaniu do niego, można umieszczać w sklepie produkty ze zdjęciami, op>
+          <p>Sklep internetowy, który zaprojektujemy i stworzymy <em>specjalnie dla Ciebie</em> będzie zaopatrzony w panel administratora. 
+              Po zalogowaniu do niego, można umieszczać w sklepie produkty ze zdjęciami, opisami i cenami oraz podzielić je na kategorie, czy 
               też tworzyć promocje według własnego uznania. </br></br>
-              W tym miejscu oraz na skrzynce mailowej będziesz otrzymywać również informacj>
+              W tym miejscu oraz na skrzynce mailowej będziesz otrzymywać również informacje o wszystkich zamówieniach Twoich klientów. 
           </p>
         </div>
       </div>
       <div class="bag_img"><img src="images/web_page_bag.png" alt=""></div>
       <div class="line_left"><img src="images/line.svg" alt=""></div>
     </div>
- <div class="block_loupe">
+    
+    <div class="block_loupe">
       <div class="sector_loupe">
         <h3 class="title_text_left">Twój wymarzony wygląd sklepu</h3>
         <div class="description_text_left">
-          <p>Doskonale wiemy, że <em>estetyczne aspekty</em> sklepu są dla Ciebie bardzo is>
+          <p>Doskonale wiemy, że <em>estetyczne aspekty</em> sklepu są dla Ciebie bardzo istotne. Opowiedz nam, jak wyobrażasz 
             sobie swój <em>wymarzony sklep</em> , a 
             my sprawimy, że stanie on przed Twoimi oczami właśnie w takiej formie.</br></br>
-            Odpowiednie i niepowtarzalne szata graficzna oraz paleta barw będą <em>dostosow>
+            Odpowiednie i niepowtarzalne szata graficzna oraz paleta barw będą <em>dostosowane do Twojej marki.</em>
           </p>
         </div>
       </div>
@@ -55,8 +56,8 @@
       <div class="sector_stocks">
         <h3 class="title_text_right">Bramka płatnicza usprawni zakupy w Twoim sklepie </h3>
         <div class="description_text_right">
-          <p>Dzięki bramce płatniczej założonej na stronie sklepu, klienci będą mogli ciesz>
-              komfortowych zakupów</em> z możliwością dokonania płatności przez Internet na>
+          <p>Dzięki bramce płatniczej założonej na stronie sklepu, klienci będą mogli cieszyć się z <em>szybkich i 
+              komfortowych zakupów</em> z możliwością dokonania płatności przez Internet na kilka <em>różnych i bezpiecznych sposobów.</em> 
           </p>
         </div>
       </div>
@@ -68,12 +69,12 @@
       <div class="sector_numbers">
         <h3 class="title_text_left">Pomożemy Ci się rozwijać</h3>
         <div class="description_text_center">
-          <p>Samo zbudowanie Twojego sklepu internetowego to nie wszystko, na co możesz lic>
-              niezbędny jest rozwój technologiczny. Jeśli potrzebna Ci będzie aktualizacja >
+          <p>Samo zbudowanie Twojego sklepu internetowego to nie wszystko, na co możesz liczyć z naszej strony. W przypadku sklepu internetowego 
+              niezbędny jest rozwój technologiczny. Jeśli potrzebna Ci będzie aktualizacja lub jakiekolwiek wsparcie, <em>zawsze jesteśmy do Twojej dyspozycji.</em>
           </p>
         </div>
       </div>
- <div class="flax_list">
+      <div class="flax_list">
         <div class="list_text">
           <div class="number_image"><img src="images/web_page_num_01.png" alt=""></div>
           <h4>Skontaktuj się z nami lub pozostaw dane kontaktowe.</h4>
@@ -107,28 +108,34 @@
       <div class="sector_ebook">
         <h3 class="title_text_right">Pobierz darmowy ebook</h3>
         <div class="description_text_center_ebook">
-          <p>Z treści ebooka <em>„10 porad na rozwój marki w Internecie”</em> się, jakie kw>
+          <p>Z treści ebooka <em>„10 porad na rozwój marki w Internecie”</em> się, jakie kwestie mają największe znaczenie przy prowadzeniu sklepu internetowego. 
           </p>
         </div>
          <div class="sector_ebook_ground"></div>
         <div class="list_ebook">
           <h2 class="title_ebook">Z poradnika dowiesz się:</h2>
           <ul>
- <li><span>Jak zwiększyć <i>rozpoznawalność</i> Twojego sklepu.</span></li>
+            <li><span>Jak zwiększyć <i>rozpoznawalność</i> Twojego sklepu.</span></li>
             <li><span>Jak utrzymać <i>dobre relacje</i> z klientami.</span></li>
             <li><span>Jakie techniki pomogą Ci osiągnąć <i>epszą sprzedaż.</i></span></li>
-            <li><span>Jak wykorzystać potencjał Internetu do <i>rozwijania marki.</i></span>
+            <li><span>Jak wykorzystać potencjał Internetu do <i>rozwijania marki.</i></span></li>
           </ul>
         </div>
       </div>
         <div class="screen_image">
           <img src="images/web_page_screen.png" alt="">
       </div>
+        <div class="btn_download">
+          <a href="index.html">POBIERZ TERAZ</a>
+        </div>
         <div class="description_text_center_ebook_down">
-          <p>Pobierz darmowego ebooka i zaproś klientów do swojego <em>wymarzonego sklepu i>
-            Aby otrzymać więcej porad i poznać więcej szczegółów na temat rozwoju swojego b>
+          <p>Pobierz darmowego ebooka i zaproś klientów do swojego <em>wymarzonego sklepu internetowego.</em></br></br>
+            Aby otrzymać więcej porad i poznać więcej szczegółów na temat rozwoju swojego biznesu w Internecie, możesz zapisać się do newslettera Human IT. 
             Dzięki któremu z czasem poznasz wszystkie tajemnice świata E-commerce.
           </p>
+        </div>
+        <div class="btn_download_biger">
+          <a href="index.html">POBIERZ TERAZ</a>
         </div>
       <div class="line_right_ebook"><img src="images/line.svg" alt=""></div>
     </div>
@@ -137,17 +144,17 @@
     <div class= "footer">
         <h3 class="title_text_left_footer"><strong>Zaprogramujemy</strong> Twój sukces!</h3>
         <form action="mail.php" method="POST">
-          <input type="text" name="user_name" placeholder="Imię i nazwisko" class="form-con>
-          <input type="text" name="user_email" placeholder="Adress e-mail" class="form-cont>
-          <input type="text" name="user_phone" placeholder="Numer telefonu" class="form-con>
-          <textarea rows="4" cols="50" type = "text" name="user_text" class="message" requi>
-          <div class="che"><input type="checkbox" class = "check_b" name ="agree"> Zgoda na>
+          <input type="text" name="user_name" placeholder="Imię i nazwisko" class="form-control" required />
+          <input type="text" name="user_email" placeholder="Adress e-mail" class="form-control" type="email" required />
+          <input type="text" name="user_phone" placeholder="Numer telefonu" class="form-control" type="phone" required />
+          <textarea rows="4" cols="50" type = "text" name="user_text" class="message" required></textarea>
+          <div class="che"><input type="checkbox" class = "check_b" name ="agree"> Zgoda na ptzetwarzanie danych osobowych</div>
         <div id="welcomeDiv"  style="display:none;" class="answer_list">
-          <button type = "button" onclick = "hideDiv()" class= "close_button">X</button>
+          <button type = "button" onclick = "hideDiv()" class= "close_button">X</button>    
         </div>
-        <button type = "submit" class= "btn btn-primary"><span class="Outsourcing1">POBIERZ TERAZ<>
+        <button type = "submit" class= "btn btn-primary"><span class="Outsourcing1">WYŚLIJ</span></button>
         </form>
-        <a href="https://www.ecommerce.humanit.group/">
+        <a href="Index.html">
         <div class="logo_footer"><img src="images/humanit_lable.svg" alt="image"></div>
       </a>
     </div>
