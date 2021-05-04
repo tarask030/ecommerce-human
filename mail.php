b12486781613b2e65bf0c74ef7190a9927c212a2
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-05-04 11:30:23 +0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,41 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$text = $_POST['user_text'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'tkram00@mail.ru'; // login
+$mail->Password = 'goAPRP2f3vr)'; // password our mail
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / this port change for each mail provider 
+
+$mail->setFrom('tkram00@mail.ru'); // mail send
+$mail->addAddress('tarask030@gmail.com');     // mail get
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Zgloszenie od klienta';
+$mail->Body    = '' .$name . ' zostawil zgloszenie, numer tefonu: ' .$phone. '<br>adres mailowy: ' .$email. ' tekst zgloszenia: <br>' .$text;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
+}
+?>
