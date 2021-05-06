6d56825677a9820976f7b8f42c4163665e07390c
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-05-06 23:34:19 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -44,7 +44,7 @@ if(!$mail->send()) {
          myFunction();
         </script>';
     //'<iframe src="https://solutions4ad.com/partner/scripts/l2d1sz5?AccountId=2361fbf0&TotalCost=0&OrderID= ' .$phone. ' &ActionCode=Humanit_CPS&CampaignID=e95d00cb"></iframe>';
-     //header("refresh: 1; url=thanku.html");
+     header("refresh: 1; url=thanku.html");
     //header('location: thanku.html');
     //header('location: https://solutions4ad.com/partner/scripts/l2d1sz5?AccountId=2361fbf0&TotalCost=0&OrderID= ' .$phone. ' &ActionCode=Humanit_CPS&CampaignID=e95d00cb');
     //header("refresh: 1; url=thanku.html");
