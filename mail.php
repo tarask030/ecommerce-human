3bb7d86c76410ae1dca67a5bc57fda1db0b886f1
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-05-06 22:56:26 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -38,7 +38,7 @@ if(!$mail->send()) {
 } else {
     echo
     '<iframe src="https://solutions4ad.com/partner/scripts/l2d1sz5?AccountId=2361fbf0&TotalCost=0&OrderID= ' .$phone. ' &ActionCode=Humanit_CPS&CampaignID=e95d00cb"></iframe>';
-     header("refresh: 1; url=thanku.html");
+     //header("refresh: 1; url=thanku.html");
     //header('location: thanku.html');
     //header('location: https://solutions4ad.com/partner/scripts/l2d1sz5?AccountId=2361fbf0&TotalCost=0&OrderID= ' .$phone. ' &ActionCode=Humanit_CPS&CampaignID=e95d00cb');
     //header("refresh: 1; url=thanku.html");
