ca283983b6342b54b34c60aad09cac0de619375d
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-05-06 22:00:07 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -45,7 +45,7 @@ function myFunction() {
 
 	</script>';
 	
-    header('location: thanku.html');
+    //header('location: thanku.html');
     //header('location: https://solutions4ad.com/partner/scripts/l2d1sz5?AccountId=2361fbf0&TotalCost=0&OrderID= ' .$phone. ' &ActionCode=Humanit_CPS&CampaignID=e95d00cb');
     //header("refresh: 1; url=thanku.html");
     //header('location: thanku.html');
